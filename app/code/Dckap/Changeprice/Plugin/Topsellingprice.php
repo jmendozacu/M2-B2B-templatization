7ead798eac98ea4a12be2bdb992e4b2e5aae6348
jothibasuj <jothibasuj@dckap.com>
2017-03-24 10:32:33 +0530

    customize custom price in topselling products

---
+++ app/code/Dckap/Changeprice/Plugin/Topsellingprice.php
@@ -0,0 +1,27 @@
+<?php
+ 
+namespace Dckap\Changeprice\Plugin;
+ 
+class Topsellingprice
+{
+	private $session;
+	public function __construct (\Magento\Customer\Model\Session $session){
+		$this->session = $session;
+	}
+	
+    public function afterGetFinalPrice(\Dckap\Topsellingitemscategory\Block\Widget\Topsellingitemscategory $subject, $result){
+    	if($this->session->isLoggedIn()) {
+    		$custom_price = '';
+			$id = $this->session->getCustomerId();
+			$objectManager = \Magento\Framework\App\ObjectManager::getInstance(); 
+			$customer = $objectManager->create('Magento\Customer\Model\Customer')->load($id);
+			$jsonoutput = $customer->getData('custom_product_price');
+			$custom_price = json_decode($jsonoutput,true);
+				if(array_key_exists($subject->getSku(),$custom_price))
+				return $custom_price[$subject->getSku()];
+				else
+				return $result;
+		}
+		return $result;
+    }
+}
