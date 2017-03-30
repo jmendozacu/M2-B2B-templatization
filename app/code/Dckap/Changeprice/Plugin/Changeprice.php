47492c62d6bd2c58de0d4721643f688f2b2f582a
harithas <harithas@dckap.com>
2017-03-30 18:02:45 +0530

    changes made in change price for checking an array

--- app/code/Dckap/Changeprice/Plugin/Changeprice.php
+++ app/code/Dckap/Changeprice/Plugin/Changeprice.php
@@ -1,7 +1,7 @@
 <?php
- 
+
 namespace Dckap\Changeprice\Plugin;
- 
+
 class Changeprice
 {
 	private $session;
@@ -9,19 +9,21 @@ class Changeprice
 		$this->session = $session;
 	}
 	
-    public function afterGetPrice(\Magento\Catalog\Model\Product $subject, $result){
-    	if($this->session->isLoggedIn()) {
-    		$custom_price = '';
+	public function afterGetPrice(\Magento\Catalog\Model\Product $subject, $result){
+		if($this->session->isLoggedIn()) {
+			$custom_price = '';
 			$id = $this->session->getCustomerId();
 			$objectManager = \Magento\Framework\App\ObjectManager::getInstance(); 
 			$customer = $objectManager->create('Magento\Customer\Model\Customer')->load($id);
 			$jsonoutput = $customer->getData('custom_product_price');
 			$custom_price = json_decode($jsonoutput,true);
+			if(is_array($custom_price)){
 				if(array_key_exists($subject->getSku(),$custom_price))
-				return $custom_price[$subject->getSku()];
+					return $custom_price[$subject->getSku()];
 				else
-				return $result;
+					return $result;
+			}
 		}
 		return $result;
-    }
+	}
 }
